58fe2425d99ec38f2456ab2038d7c5440f10034d
ASAPH BONIFACIO GUIMARAES ARRUDA <20241014040010@ifrn.local>
2024-10-14 10:30:56 -0300

    MUD: Arquivo 10

--- Exercicio_10.asm
+++ Exercicio_10.asm
@@ -1,23 +1,37 @@
 .text
-#Faça um programa que leia 1 inteiro entre 100 e 999 e o imprima escrito de trás para
-#frente. Exemplo: 384 gera uma saída 483
+#Faça um programa que leia um caractere 
+#minúsculo e imprima o seu equivalente
+#maiúsculo.
 
 main:
-	addi $10 $0 100
-	addi $11 $0 10
-	
 	# INÍCIO
 	# IMPRESSÃO NÚMERO
 	
-	add $4 $0 'N'
+	add $4 $0 'C'
 	addi $2 $0 11
 	syscall
 	
-	add $4 $0 'U'
+	add $4 $0 'H'
 	addi $2 $0 11
 	syscall
 	
-	add $4 $0 'M'
+	add $4 $0 'A'
+	addi $2 $0 11
+	syscall
+	
+	add $4 $0 'R'
+	addi $2 $0 11
+	syscall
+	
+	add $4 $0 'A'
+	addi $2 $0 11
+	syscall
+	
+	add $4 $0 'C'
+	addi $2 $0 11
+	syscall
+	
+	add $4 $0 'T'
 	addi $2 $0 11
 	syscall
 	
@@ -26,10 +40,6 @@ main:
 	syscall
 	
 	add $4 $0 'R'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 'O'
 	addi $2 $0 11
 	syscall
 	
@@ -44,85 +54,14 @@ main:
 	# IMPRESSÃO NÚMERO
 	# FIM
 	
-	
-	# INPUT
-	addi $2 $0 5
+	addi $2 $0 12
 	syscall
-	add $15 $0 $2
+	add $4 $0 $2
 	
-	# FIM INPUT
-	
-	div $15 $10
-	mflo $15
-	mfhi $16
-	
-	div $16 $11
-	mflo $16
-	mfhi $17
-	
-	# INICIO
-	# IMPRESSÃO NUM_INVER.
-	
-	add $4 $0 'N'
+	addi $4 $0 -32
 	addi $2 $0 11
 	syscall
 	
-	add $4 $0 'U'
-	addi $2 $0 11
-	syscall
 	
-	add $4 $0 'M'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 '_'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 'I'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 'N'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 'V'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 'E'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 'R'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 '.'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 ':'
-	addi $2 $0 11
-	syscall
-	
-	add $4 $0 ' '
-	addi $2 $0 11
-	syscall
-	
-	# IMPRESSÃO NUM_INVER.
-	# FIM
-	
-	add $4 $0 $17
-	addi $2 $0 1
-	syscall
-	
-	add $4 $0 $16
-	addi $2 $0 1
-	syscall
-	
-	add $4 $0 $15
-	addi $2 $0 1
-	syscall
+
 	
