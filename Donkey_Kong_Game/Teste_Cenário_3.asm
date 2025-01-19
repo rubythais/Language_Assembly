4d289c00fec991403b690eb857aeaf752fcbfdd1
Asaph Arruda <asapharruda@gmail.com>
2025-01-19 20:35:23 -0300

    Add: Cenário 3

---
+++ Donkey_Kong_Game/Teste_Cenário_3.asm
@@ -0,0 +1,980 @@
+.text
+
+main:
+addi $3 $0 0 
+    addi $9 $0 0 
+    ori $9 0xCACACA 
+    jal Mario_Dark_Sky_Draw
+
+ 
+
+    addi $3 $0 1 
+    addi $9 $0 0 
+    ori $9 0xD5D5D5 
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 2
+    addi $9 $0 0
+    ori $9 0xD1D1D1
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 3
+    addi $9 $0 0
+    ori $9 0xCDCDCD
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 4
+    addi $9 $0 0
+    ori $9 0xC9C9C9
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 5
+    addi $9 $0 0
+    ori $9 0xC5C5C5
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 6
+    addi $9 $0 0
+    ori $9 0xC1C1C1
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 7
+    addi $9 $0 0
+    ori $9 0xBDBDBD
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 8
+    addi $9 $0 0
+    ori $9 0xB9B9B9
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 9
+    addi $9 $0 0
+    ori $9 0xB4B4B4
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 10
+    addi $9 $0 0
+    ori $9 0xB0B0B0
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 11
+    addi $9 $0 0
+    ori $9 0xACACAC
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 12 
+    addi $9 $0 0
+    ori $9 0xA8A8A8
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 13
+    addi $9 $0 0
+    ori $9 0xA4A4A4
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 14
+    addi $9 $0 0
+    ori $9 0xA0A0A0
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 15
+    addi $9 $0 0
+    ori $9 0x9C9C9C
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 16
+    addi $9 $0 0
+    ori $9 0x989898
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 17
+    addi $9 $0 0
+    ori $9 0x959595
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 18
+    addi $9 $0 0
+    ori $9 0x919191
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 19
+    addi $9 $0 0
+    ori $9 0x8E8E8A
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 20
+    addi $9 $0 0
+    ori $9 0x8A8A8A
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 21
+    addi $9 $0 0
+    ori $9 0x858585
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 22
+    addi $9 $0 0
+    ori $9 0x828282
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 23
+    addi $9 $0 0
+    ori $9 0x7E7E7E
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 24
+    addi $9 $0 0
+    ori $9 0x797979
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 25
+    addi $9 $0 0
+    ori $9 0x757575
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 26
+    addi $9 $0 0
+    ori $9 0x717171
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 27
+    addi $9 $0 0
+    ori $9 0x6E6E6E
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 28
+    addi $9 $0 0
+    ori $9 0x6A6A6A
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 29
+    addi $9 $0 0
+    ori $9 0x656565
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 30 
+    addi $9 $0 0
+    ori $9 0x616161
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 31
+    addi $9 $0 0
+    ori $9 0x5E5E5E
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 32
+    addi $9 $0 0
+    ori $9 0x5A5A5A
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 33
+    addi $9 $0 0
+    ori $9 0x555555
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 34 
+    addi $9 $0 0
+    ori $9 0x515151
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 35
+    addi $9 $0 0
+    ori $9 0x4C4C4C
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 36
+    addi $9 $0 0
+    ori $9 0x494949
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 37
+    addi $9 $0 0
+    ori $9 0x454545
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 38
+    addi $9 $0 0
+    ori $9 0x3A3A3A
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 39
+    addi $9 $0 0
+    ori $9 0x400000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 40
+    addi $9 $0 0
+    ori $9 0x3F0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 41
+    addi $9 $0 0
+    ori $9 0x3D0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 42 
+    addi $9 $0 0
+    ori $9 0x3B0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 43
+    addi $9 $0 0
+    ori $9 0x390000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 44
+    addi $9 $0 0
+    ori $9 0x370000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 45
+    addi $9 $0 0
+    ori $9 0x350000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 46
+    addi $9 $0 0
+    ori $9 0x330000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 47
+    addi $9 $0 0
+    ori $9 0x310000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 48
+    addi $9 $0 0
+    ori $9 0x2F0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 49
+    addi $9 $0 0
+    ori $9 0x2D0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 50
+    addi $9 $0 0
+    ori $9 0x2B0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 51
+    addi $9 $0 0
+    ori $9 0x030000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 52
+    addi $9 $0 0
+    ori $9 0x090000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 53
+    addi $9 $0 0
+    ori $9 0x0F0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 54
+    addi $9 $0 0
+    ori $9 0x150000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 55
+    addi $9 $0 0
+    ori $9 0x1B0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 56
+    addi $9 $0 0
+    ori $9 0x210000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 57
+    addi $9 $0 0
+    ori $9 0x280000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 58
+    addi $9 $0 0
+    ori $9 0x2E0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 59
+    addi $9 $0 0
+    ori $9 0x340000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 60
+    addi $9 $0 0
+    ori $9 0x3A0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 61
+    addi $9 $0 0
+    ori $9 0x400000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 62
+    addi $9 $0 0
+    ori $9 0x470000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 63
+    addi $9 $0 0
+    ori $9 0x4D0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 64
+    addi $9 $0 0
+    ori $9 0x530000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 65
+    addi $9 $0 0
+    ori $9 0x590000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 66
+    addi $9 $0 0
+    ori $9 0x600000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 67
+    addi $9 $0 0
+    ori $9 0x660000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 68
+    addi $9 $0 0
+    ori $9 0x6C0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 69
+    addi $9 $0 0
+    ori $9 0x720000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 70
+    addi $9 $0 0
+    ori $9 0x790000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 71
+    addi $9 $0 0
+    ori $9 0x7F0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 72
+    addi $9 $0 0
+    ori $9 0x850000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 73
+    addi $9 $0 0
+    ori $9 0x8B0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 74 
+    addi $9 $0 0
+    ori $9 0x910000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 75
+    addi $9 $0 0
+    ori $9 0x980000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 76
+    addi $9 $0 0
+    ori $9 0x9E0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 77
+    addi $9 $0 0
+    ori $9 0xA40000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 78
+    addi $9 $0 0
+    ori $9 0xAA0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 79
+    addi $9 $0 0
+    ori $9 0xB10000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 80
+    addi $9 $0 0
+    ori $9 0xB70000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 81
+    addi $9 $0 0
+    ori $9 0xBD0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 82
+    addi $9 $0 0
+    ori $9 0xC30000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 83 
+    addi $9 $0 0
+    ori $9 0xC90000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 84
+    addi $9 $0 0
+    ori $9 0xCF0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 85
+    addi $9 $0 0
+    ori $9 0xD50000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 86
+    addi $9 $0 0
+    ori $9 0xDB0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 87
+    addi $9 $0 0
+    ori $9 0xE10000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 88
+    addi $9 $0 0
+    ori $9 0xE70000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 89
+    addi $9 $0 0
+    ori $9 0xED0000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 90
+    addi $9 $0 0
+    ori $9 0xF30000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 91
+    addi $9 $0 0
+    ori $9 0xF90000
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 92
+    addi $9 $0 0
+    ori $9 0xFF0303
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 93
+    addi $9 $0 0
+    ori $9 0xFF0909
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 94
+    addi $9 $0 0
+    ori $9 0xFF0F0F
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 95
+    addi $9 $0 0
+    ori $9 0xFF1515
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 96
+    addi $9 $0 0
+    ori $9 0xFF1B1B
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 97
+    addi $9 $0 0
+    ori $9 0xFF2121
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 98
+    addi $9 $0 0
+    ori $9 0xFF2828
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 99
+    addi $9 $0 0
+    ori $9 0xFF2E2E
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 100
+    addi $9 $0 0
+    ori $9 0xFF3434
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 101 
+    addi $9 $0 0
+    ori $9 0xFF3A3A
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 102
+    addi $9 $0 0
+    ori $9 0xFF4040
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 103
+    addi $9 $0 0
+    ori $9 0xFF4747
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 104
+    addi $9 $0 0
+    ori $9 0xFF4D4D
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 105
+    addi $9 $0 0
+    ori $9 0xFF5353
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 106
+    addi $9 $0 0
+    ori $9 0xFF5959
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 107
+    addi $9 $0 0
+    ori $9 0xFF6060
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 108
+    addi $9 $0 0
+    ori $9 0xFF6666
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 109
+    addi $9 $0 0
+    ori $9 0xFF6C6C
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 110
+    addi $9 $0 0
+    ori $9 0xFF7272
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 111
+    addi $9 $0 0
+    ori $9 0xFF7979
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 112
+    addi $9 $0 0
+    ori $9 0xFF7F7F
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 113
+    addi $9 $0 0
+    ori $9 0xFF8585
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 114
+    addi $9 $0 0
+    ori $9 0xFF8B8B
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 115
+    addi $9 $0 0
+    ori $9 0xFF9191
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 116
+    addi $9 $0 0
+    ori $9 0xFF9898
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 117
+    addi $9 $0 0
+    ori $9 0xFF9E9E
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 118
+    addi $9 $0 0
+    ori $9 0xFFA4A4
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 119
+    addi $9 $0 0
+    ori $9 0xFFAAAA
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 120
+    addi $9 $0 0
+    ori $9 0xFFB1B1
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 121
+    addi $9 $0 0
+    ori $9 0xFFB7B7
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 122
+    addi $9 $0 0
+    ori $9 0xFFBDBD
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 123
+    addi $9 $0 0
+    ori $9 0xFFC3C3
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 124 
+    addi $9 $0 0
+    ori $9 0xFFCACA
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 125
+    addi $9 $0 0
+    ori $9 0xFFD0D0
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 126
+    addi $9 $0 0
+    ori $9 0xFFD6D6
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 127
+    addi $9 $0 0
+    ori $9 0xFFDCDC
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 128
+    addi $9 $0 0
+    ori $9 0xFFE3E3
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 129
+    addi $9 $0 0
+    ori $9 0xFFE9E9
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 130
+    addi $9 $0 0
+    ori $9 0xFFEFEF
+    jal Mario_Dark_Sky_Draw
+
+
+
+    addi $3 $0 131
+    addi $9 $0 0
+    ori $9 0xFFF5F5
+    jal Mario_Dark_Sky_Draw
+
+    jal andar
+
+
+
+fim:
+	addi $2 $0 10
+	syscall
+
+
+# ================================================================
+# **** Desenhar Detalhes ****
+
+# INPUT_Reg: $2 -> Tamanho dos Datalhes
+#            $3 -> Posição Y 
+# 	     $9 -> Cor 	
+# OUTPUT_Reg: None
+# Reg_Usados:
+Mario_Dark_Sky_Draw:
+	sw $31 -4($29)
+	lui $8 0x1001
+	addi $4 $0 1024
+	mul $4 $4 $3
+	add $8 $8 $4
+	addi $10 $0 0
+	
+Mario_Dark_Sky_Draw_For:
+	beq $10 256 fim_Mario_Dark_Sky_Draw
+	sw $9 0($8)
+	sw $9 262144($8)
+	addi $8 $8 4
+	addi $10 $10 1
+	j Mario_Dark_Sky_Draw_For
+
+fim_Mario_Dark_Sky_Draw:
+	lw $31 -4($29)
+	jr $31
+	
+	
+andar:
+	lui $8 0x1001
+	addi $9 $0 0
+	ori $9 0xCA9D40
+	addi $10 $0 8000
+
+for: beq $10, $0, fim      
+     
+      sw $9, 0($8)
+      sw $9, 4($8)
+      lw $20, 262144($8)
+      sw $20, 0($8)
+      addi $8, $8, 4
+ 
+
+      addi $10, $10, -1
+      j for
