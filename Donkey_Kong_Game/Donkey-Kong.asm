477cc42b7cbfa307f30f70f05aba1f0d1bb315fe
Asaph Arruda <160978598+ArrudaAsaph@users.noreply.github.com>
2025-01-31 10:28:53 -0300

    Update Donkey-Kong.asm

--- Donkey_Kong_Game/Donkey-Kong.asm
+++ Donkey_Kong_Game/Donkey-Kong.asm
@@ -188,7 +188,7 @@ Donkey_Kong:
 	ori $9 0x9E4000
 	jal Donkey_Kong_Draw_Pixel
 	
-	addi $2 $0 20
+	addi $2 $0 18
 	addi $3 $0 2
 	addi $4 $15 8
 	addi $5 $16 44
@@ -227,7 +227,61 @@ Donkey_Kong:
 	addi $9 $0 0
 	ori $9 0x9E4000
 	jal Donkey_Kong_Draw_Pixel
-	# PÉ TRAZEIRO
+	
+	# PERNA DIANTEIRA MARROM
+	addi $2 $0 14
+	addi $3 $0 4
+	addi $4 $15 30
+	addi $5 $16 48
+	addi $9 $0 0
+	ori $9 0x9E4000
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 10
+	addi $3 $0 2
+	addi $4 $15 32
+	addi $5 $16 52
+	addi $9 $0 0
+	ori $9 0x9E4000
+	jal Donkey_Kong_Draw_Pixel
+	
+	# PERNA DIANTEIRA
+	
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 38
+	addi $5 $16 42
+	addi $9 $0 0
+	ori $9 0xF6CE9B
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 6
+	addi $3 $0 4
+	addi $4 $15 36
+	addi $5 $16 44
+	addi $9 $0 0
+	ori $9 0xF6CE9B
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 10
+	addi $3 $0 4
+	addi $4 $15 32
+	addi $5 $16 48
+	addi $9 $0 0
+	ori $9 0xF6CE9B
+	jal Donkey_Kong_Draw_Pixel
+	
+	# OUTRA PERNA
+	addi $2 $0 6
+	addi $3 $0 6
+	addi $4 $15 44
+	addi $5 $16 46
+	addi $9 $0 0
+	ori $9 0xF6CE9B
+	jal Donkey_Kong_Draw_Pixel
+	
+	
+	# PERNA TRAZEIRO
 	addi $2 $0 14
 	addi $3 $0 2
 	addi $4 $15 10
@@ -252,7 +306,8 @@ Donkey_Kong:
 	ori $9 0xF6CE9B
 	jal Donkey_Kong_Draw_Pixel
 	
-	# PÉ TRAZEIRO DETALHES
+	# PERNA TRAZEIRO DETALHES
+	# ** PRETO **
 	addi $2 $0 4
 	addi $3 $0 4
 	addi $4 $15 24
@@ -284,7 +339,136 @@ Donkey_Kong:
 	addi $9 $0 0
 	ori $9 0x00000
 	jal Donkey_Kong_Draw_Pixel
+	# ** MARROM ESCURO ** #531B02
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 16
+	addi $5 $16 44
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
 	
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 24
+	addi $5 $16 44
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 4
+	addi $3 $0 2
+	addi $4 $15 18
+	addi $5 $16 42
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 26
+	addi $5 $16 42
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 2
+	addi $3 $0 4
+	addi $4 $15 20
+	addi $5 $16 38
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	# PARTE CIMA DO DETALHE DA PERNA TRAZEIRA
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 22
+	addi $5 $16 38
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 4
+	addi $3 $0 2
+	addi $4 $15 14
+	addi $5 $16 34
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 18
+	addi $5 $16 36
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	# END - PARTE CIMA DO DETALHE DA PERNA TRAZEIRA
+	
+	addi $2 $0 2
+	addi $3 $0 4
+	addi $4 $15 28
+	addi $5 $16 38
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 6
+	addi $3 $0 2
+	addi $4 $15 24
+	addi $5 $16 36
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 4
+	addi $3 $0 2
+	addi $4 $15 24
+	addi $5 $16 34
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 4
+	addi $3 $0 4
+	addi $4 $15 22
+	addi $5 $16 30
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 2
+	addi $3 $0 6
+	addi $4 $15 22
+	addi $5 $16 24
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 2
+	addi $3 $0 2
+	addi $4 $15 24
+	addi $5 $16 22
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	# DETLAHE PERNA DIANTEIRA #531B02
+	addi $2 $0 2
+	addi $3 $0 4
+	addi $4 $15 30
+	addi $5 $16 40
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
+	
+	addi $2 $0 2
+	addi $3 $0 4
+	addi $4 $15 32
+	addi $5 $16 44
+	addi $9 $0 0
+	ori $9 0x531B02
+	jal Donkey_Kong_Draw_Pixel
 	####
 	addi $2 $0 6
 	addi $3 $0 50
@@ -322,8 +506,8 @@ Donkey_Kong_Draw_Pixel:
 	
 	lui $8 0x1001 # Memoria
 	addi $6 $0 1024
-	mul $6 $6 $5 #Posição Y
-	mul $7 $4 4 # Posição X
+	mul $6 $6 $5 #PosiÃ§Ã£o Y
+	mul $7 $4 4 # PosiÃ§Ã£o X
 	add $8 $8 $6
 	add $8 $8 $7
 	addi $5 $0 0 # Contador 1
